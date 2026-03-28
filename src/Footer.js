1ed7c513a12ac65dfb2c55012e7bd158bf743de2
anasdkil336699-dot <anasdkil336699@gmail.com>
2026-03-28 07:02:28 +0300

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <p>Copyright &copy; 2025 - [Your Name]</p>
+      <p>Copyright &copy; 2025 - [ِAnas Al-Suhaymi]</p>
     </footer>
   );
 }
